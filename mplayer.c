d05d9505e0ed5feb14076a5ec37a8a944a7691dd
Kovensky <kovensk@gmail.com>
2009-07-30 19:42:42 -0300

    Do not mute the mixer at exit if the user had muted the player
    Based on http://oss.netfarm.it/mplayer/patches/xx_nomute_atexit.diff

--- mplayer.c
+++ mplayer.c
@@ -674,7 +674,6 @@ void uninit_player(struct MPContext *mpctx, unsigned int mask){
 
 void exit_player_with_rc(struct MPContext *mpctx, exit_reason_t how, int rc)
 {
-  if (mpctx->user_muted && !mpctx->edl_muted) mixer_mute(&mpctx->mixer);
   uninit_player(mpctx, INITIALIZED_ALL);
 #if defined(__MINGW32__) || defined(__CYGWIN__)
   timeEndPeriod(1);
