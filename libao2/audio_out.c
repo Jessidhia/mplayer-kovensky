e9bf7dc791b3dba53c331f4e944728f668663e38
Kovensky <diogomfranco@gmail.com>
2011-08-27 19:44:15 +0000

    Do not disable the OpenAL test by default
    Prefer OpenAL over other AOs if available.

--- libao2/audio_out.c
+++ libao2/audio_out.c
@@ -58,6 +58,9 @@ extern const struct ao_driver audio_out_pcm;
 extern const struct ao_driver audio_out_pss;
 
 static const struct ao_driver * const audio_out_drivers[] = {
+#ifdef CONFIG_OPENAL
+    &audio_out_openal,
+#endif
 // native:
 #ifdef CONFIG_DIRECTX
     &audio_out_dsound,
@@ -107,9 +110,6 @@ static const struct ao_driver * const audio_out_drivers[] = {
 #endif
 #ifdef CONFIG_SDL
     &audio_out_sdl,
-#endif
-#ifdef CONFIG_OPENAL
-    &audio_out_openal,
 #endif
     &audio_out_mpegpes,
 #ifdef CONFIG_IVTV
